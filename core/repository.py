eca27a03f8862a2b12f908ef148aa17b39d3d1d4
yuta <yuta1152704@gmail.com>
2026-05-14 17:34:28 +0900

    feat:DBに重複データが登録できないように変更

--- core/repository.py
+++ core/repository.py
@@ -8,7 +8,7 @@ from typing import Dict, Tuple
 BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / "data" / "projects.db"
 
-PROJECT_COLUMNS = ["keyword", "title", "tags", "money", "contract", "location", "station", "skills", "occupations", "update_date", "publisher"]
+PROJECT_COLUMNS = ["keyword", "title", "tags", "money", "contract", "location", "station", "skills", "occupations", "update_date", "publisher", "project_id"]
 
 def get_connection() -> sqlite3.Connection:
     DB_PATH.parent.mkdir(parents=True, exist_ok=True)
@@ -31,7 +31,8 @@ def init_db() -> None:
             skills TEXT NOT NULL,
             occupations TEXT NOT NULL,
             update_date TEXT NOT NULL,
-            publisher TEXT NOT NULL
+            publisher TEXT NOT NULL,
+            project_id TEXT NOT NULL UNIQUE
         )
         """)
 
@@ -49,22 +50,34 @@ def to_project_row(project:Dict) -> Tuple:
     # print(f"tuple_project: {tuple_project}")
     return tuple_project
 
+def filter_new_projects(projects:list[Dict]) -> list[Dict]:
+    existing_keys = get_key_from_projects()
+    print(f"{existing_keys=}")
+    new_projects = []
+
+    for project in projects:
+        key = project["project_id"]
+        if key not in existing_keys:
+            new_projects.append(project)
+    
+    return new_projects
+
 def insert_projects(projects:list[Dict]) -> None:
     with get_connection() as conn:
         cursor = conn.cursor()
 
-        rows = [to_project_row(project) for project in projects]
-        print(f"rows:{rows}")
+        insert_rows = [to_project_row(project) for project in projects]
+        print(f"{insert_rows=}")
 
         query = f"""
-                INSERT
+                INSERT OR IGNORE
                 INTO projects (
                   {','.join(PROJECT_COLUMNS)}
                 )
                 VALUES ({','.join(['?'] * len(PROJECT_COLUMNS))})
                 """
         
-        cursor.executemany(query, rows)
+        cursor.executemany(query, insert_rows)
 
 def get_projects():
     with get_connection() as conn:
@@ -72,3 +85,11 @@ def get_projects():
         cursor.execute("SELECT * FROM projects")
         rows = cursor.fetchall()
     return rows
+
+def get_key_from_projects() -> set[str]:
+    with get_connection() as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT project_id FROM projects")
+        rows = cursor.fetchall()
+    return set(row[0] for row in rows)
